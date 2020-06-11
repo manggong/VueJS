e77c3df52f3dd49e866891bb1a3d663e76ef43ac
manggong <jihwank1995@gmail.com>
2020-06-11 15:44:33 +0900

    vue-cli tutorial

---
+++ cli-test/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="나는 김지환이다!!!" />
+    <TodoList :todos="todos" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import TodoList from "./components/TodoList.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    TodoList
+  },
+  data() {
+    return {
+      todos: [
+        { id: 1, content: "Vue Vue Vue", isCompleted: false },
+        { id: 2, content: "잠자기", isCompleted: false },
+        { id: 3, content: "밥먹기", isCompleted: false },
+        { id: 4, content: "계속자기", isCompleted: false }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ cli-test/src/components/TodoList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Todo List의 List들 입니다!!!</h1>
+    <p>{{title}}</p>
+    <ul>
+      <li v-for="todo in todos" v-bind:key="todo.id">{{todo.content}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  data() {
+    return {
+      title: "This is title"
+    };
+  },
+  props: {
+    todos: Array
+  }
+};
+</script>
+
+<style>
+</style>
